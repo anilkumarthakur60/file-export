130bc57422c7059fe3a9b623c31e9ed471e35e8c
anil kumar thakur <anilkumarthakur60@gmail.com>
2023-08-25 23:21:42 +0545

    inital commit

---
+++ src/Service/CommonExport.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Anil\FileExport\Service;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Events\AfterSheet;
+
+class CommonExport implements FromCollection, WithMapping, WithHeadings, WithEvents
+{
+    public function __construct(public $model, public $headers, public $mapping)
+    {
+    }
+
+    public function headings(): array
+    {
+        return $this->headers;
+    }
+
+    public function collection():Collection
+    {
+        return $this->model->map(function ($item, $index) {
+            $item->s_no = $index + 1;
+            return $item;
+        });
+    }
+
+    public function map($row): array
+    {
+        return call_user_func($this->mapping, $row);
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $lastRow = $event->sheet->getDelegate()->getHighestRow();
+                $event->sheet->getDelegate()->getStyle('A1:Z'.$lastRow)->getAlignment()->setWrapText(true);
+            },
+        ];
+    }
+}
---
+++ src/Traits/ExportDefaultMethods.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Anil\FileExport\Traits;
+
+use Anil\FileExport\Service\CommonExport;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Facades\Excel;
+use ReflectionException;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+trait ExportDefaultMethods
+{
+
+
+    private function finalExport(string $exportName, $exportClass): JsonResponse
+    {
+        $path = 'export/'.$exportName;
+        $export = Excel::store($exportClass, $path, config('filesystems.default'));
+
+        if ($export) {
+            return response()->json([
+                'code' => 200,
+                'url' => Storage::disk('public')->url($path),
+                'message' => 'Success',
+            ], ResponseAlias::HTTP_OK);
+        }
+
+        return response()->json([
+            'code' => 500,
+            'message' => 'Something went wrong',
+        ], ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    public function commonExport($model, $header, $mapping): CommonExport
+    {
+        return new CommonExport($model, $header, $mapping);
+    }
+
+    private function paginatedData($model)
+    {
+        request()->validate([
+            'exportRows' => 'nullable|numeric|lte:100000000|gte:0',
+        ]);
+        return $model->paginate(request()->query('exportRows',$model->count()));
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function exportName($modelClass): string
+    {
+        if (request()->filled('exportName')) {
+            return request()->input('exportName').time().'.xlsx';
+        }
+
+        $reflection = new \ReflectionClass(app($modelClass));
+        $className= $reflection->getShortName();
+
+        return Str::slug($className).time().'.xlsx';
+    }
+
+
+    /**
+     * @throws ReflectionException
+     */
+    public function exportData($builder, $header, $mapping): JsonResponse
+    {
+        $exportClass = $this->commonExport(
+            model: $this->paginatedData(model: $builder),
+            header: $header,
+            mapping: $mapping,
+        );
+
+        $exportName = $this->exportName(modelClass: get_class($builder->getModel()));
+        return $this->finalExport(exportName: $exportName, exportClass: $exportClass);
+    }
+}
